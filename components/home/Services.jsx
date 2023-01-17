e309b8a76315024656ebd721114c1330741caa54
Redwan Ahmed <redwanahmed.dev@gmail.com>
2023-01-17 02:30:57 -0500

    Update Services.jsx

--- components/home/Services.jsx
+++ components/home/Services.jsx
@@ -17,9 +17,9 @@ const Services = () => {
   return (
     <>
       <div
-        className="service__Section max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+        className="service__Section mx-auto mt-8 mb-6 max-w-screen-xl px-6 sm:mt-14 sm:mb-14 sm:px-8 lg:px-16"
         id="feature">
-        <div className="serviceGrid__container grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
+        <div className="serviceGrid__container p y-8 my-12 grid grid-flow-row grid-cols-1 gap-8  sm:grid-flow-col sm:grid-cols-2">
           <ScrollAnimationWrapper className="serviceGrid__imageWrapper flex w-full justify-end">
             <motion.div
               className="serviceGrid__image h-full w-full p-4"
@@ -37,19 +37,19 @@ const Services = () => {
 
           <ScrollAnimationWrapper>
             <motion.div
-              className="serviceGrid__textWrapper flex flex-col items-end justify-center ml-auto w-full lg:w-9/12"
+              className="serviceGrid__textWrapper ml-auto flex w-full flex-col items-end justify-center lg:w-9/12"
               variants={scrollAnimation}>
-              <h3 className="serviceGrid__textTitle text-2xl  md:text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+              <h3 className="serviceGrid__textTitle text-2xl  font-medium leading-relaxed text-black-600 md:text-3xl lg:text-4xl">
                 Find the Most Convenient Services on BuyMyTime
               </h3>
               <p className="serviceGrid__textPara my-2 text-black-500">
                 Explore a variety of services offered by our world-class
                 professionals on BuyMyTime.
               </p>
-              <ul className="serviceText__services text-black-500 self-start list-inside ml-8">
+              <ul className="serviceText__services ml-8 list-inside self-start text-black-500">
                 {services.map((service, index) => (
                   <motion.li
-                    className="serviceText__service relative circle-check custom-list"
+                    className="serviceText__service circle-check custom-list relative"
                     custom={{ duration: 1 + index }}
                     variants={scrollAnimation}
                     key={index}
