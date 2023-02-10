98cffd64b34a3acdd50a89b4b9e72dca4bddcb8f
Redwan Ahmed <redwanahmed.dev@gmail.com>
2023-02-10 00:24:08 -0500

    testing

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoBMT from "../../public/assets/Logo.svg";
+import Logo from "../../public/assets/Logo.svg";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="footer bg-white-300 pt-24 sm:pb-24">
       <div className="footer__container max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="footer__essentials row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start">
-          <LogoBMT className="footer__logo h-12 w-auto mb-6" />
+          <Logo className="footer__logo h-12 w-auto mb-6" />
           <p className="footer__essentialsText mb-4">
             <strong className="font-medium">BuyMyTime</strong>: your go-to for
             companionship and convinience.
--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from "./Footer";
+import Footer from "/components/layout/Footer.jsx";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
