896ab4148d51f90bbb049207522dc8502a51ebe1
Redwan Ahmed <redwanahmed.dev@gmail.com>
2023-02-11 18:18:49 -0500

    updated sorting review

--- pages/api/getServices.js
+++ pages/api/getServices.js
@@ -6,7 +6,7 @@ const servicesData = groq`*[_type == 'service']{
   seller->{'id':_id, name, sellerImageUrl, sellerRating},
   'reviews': *[_type == 'review' && references(^._id) ]{
      'id':_id,'reviewer':name,rating,review,'created':_createdAt,
-      }
+      } | order(created desc)
   }`;
 export default async function handler(request, response) {
   const services = await sanityClient.fetch(servicesData);
